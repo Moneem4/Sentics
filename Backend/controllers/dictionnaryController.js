a30c80b2bc46c2ce1c05be0749da1f39b3350319
Moneem44 <monemmtiraoui1995@gmail.com>
2023-01-20 10:44:04 +0100

    update response apis

--- Backend/controllers/dictionnaryController.js
+++ Backend/controllers/dictionnaryController.js
@@ -1,5 +1,4 @@
 
-const dictionnaryModel = require('../models/dictionnarySchema');
 const daysjs = require('dayjs')
 //make connection to our database and collections
 const { MongoClient } = require("mongodb");
@@ -10,53 +9,29 @@ const db = client.db("Sentics");
 const coll = db.collection("dictionnaries");
  //dictionnary controller
 /*********************************************************************************************/ 
-// find all dictionnaries
-exports.findAlldictionnaries = async (req, res) => {
-    //add try catch to my code
-  try {
-    const dictionnaries = await coll.find().limit(100).toArray()
-    //initialise an empty array for the find result
-    let dics =[]
-    //add elements to our array from the find command
-    console.log('****',dictionnaries ,'*******')
-    // return result with the 200 statusCode
-    res.status(200).json({ message: "success", data:dictionnaries});
-} catch(error){ 
-  console.log(error)
-    //in case of server error
-    res.status(500).json({
-    message: "Failure",
-    data: {
-        errorMessage: "Server error !"
-    }
-});}
-}
-
- 
-  //positionsOfHumans
-exports.getDataByTime = async (req, res) => {
+  //numberOfHumans
+exports.getNumberOfHumansByTime = async (req, res) => {
     try {
-      //destruct time input
-      const { time } = req.body;
-      //convert input time to date type
-      const dateEnd = daysjs(time).add(1,'minute').toDate()
-      console.log('dateEnd',);
+      //destruct time1 and time2  inputs
+      const { time1,time2 } = req.body;
+      //find dictionnaries by time interval
+      
       const dictionnaries =await coll.find({$and:
         [
-          {timestamp:{$gt:new Date(time)}},
-          {timestamp:{$lt:dateEnd}}
+          {timestamp:{$gt:new Date(time1)}},
+          {timestamp:{$lt:new Date(time2)}}
        ]
      
       }).toArray()
-      let positions = []
+      let result = []
        dictionnaries.forEach(elt=> {
         const instances = elt.instances
-        Object.entries(instances).map(instance=> {
-          const [key,value] = instance
-          positions.push(value.pos_x)
-        })
+        let  time=elt.timestamp
+        let numberHumans= Object.entries(instances).length
+        console.log("number ",numberHumans)
+        result.push({numberHumans,time})
       }) 
-      res.status(200).json({ message: "success", data:{ positions, count:dictionnaries.length} });
+      res.status(200).json({ message: "success", data:result });
     } catch (error) {
         console.log(error)
         res.status(500).json({
@@ -67,7 +42,41 @@ exports.getDataByTime = async (req, res) => {
         });
     }
   };
-
+//positionsOfHumans
+exports.getPositionsByTime = async (req, res) => {
+  try {
+    //destruct time1 and time 2  inputs
+    const { time1,time2 } = req.body;
+    
+    const dictionnaries =await coll.find({$and:
+      [
+        {timestamp:{$gt:new Date(time1)}},
+        {timestamp:{$lt:new Date(time2)}}
+     ]
+   
+    }).toArray()
+    let positions = []
+     dictionnaries.forEach(elt=> {
+      const instances = elt.instances
+      Object.entries(instances).map(instance=> {
+        const [key,value] = instance
+        px=value.pos_x;
+        py=value.pos_y;
+       vy=value.vel_y
+         positions.push({px,py,vy})
+      })
+    }) 
+    res.status(200).json({ message: "success", data:{ positions} });
+  } catch (error) {
+      console.log(error)
+      res.status(500).json({
+          message: "Failure",
+          data: {
+              errorMessage: "Server error !"
+          }
+      });
+  }
+};
 
 
 
--- Backend/routes/dictionnaryRoute.js
+++ Backend/routes/dictionnaryRoute.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { findAlldictionnaries,getDataByTime} = require('../controllers/dictionnaryController');
+const { getNumberOfHumansByTime,getPositionsByTime} = require('../controllers/dictionnaryController');
 
 // all dictionnary's routes
-// find all dictionnaries 
-router.get('/findAlldictionnaries', findAlldictionnaries);
 
-router.get('/getDataByTime', getDataByTime);
+router.get('/getNumberOfHumansByTime', getNumberOfHumansByTime);
+router.get('/getPositionsByTime', getPositionsByTime);
 module.exports = router;
